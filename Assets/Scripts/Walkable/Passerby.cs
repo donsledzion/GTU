1f1b88b2d7e45558a7a02ec277a48191b33c42c5
DESKTOP-K94QVJ7\choja <adam.chojaczyk@epicvr.pl>
2022-02-09 23:10:41 +0100

    Passerby can now follow path given by transforms array

--- Assets/Scripts/Walkable/Passerby.cs
+++ Assets/Scripts/Walkable/Passerby.cs
@@ -5,28 +5,58 @@ using UnityEngine;
 public class Passerby : Human
 {
     [SerializeField]GameObject target;
+    [SerializeField]Transform[] targetSet;
     [SerializeField] float angleBetweenVectors;
-    
-    // Update is called once per frame
+    [SerializeField] bool isColliding;
+    [Range(0.3f, 1.0f)]
+    [SerializeField] float speedFactor = 0.5f;
+
+    int currentTarget = 0;
+        
+        
     void Update()
     {
         if(target)
+            NavigateToTarget(target.transform);
+        if (targetSet.Length > 0)
+            NavigateThroughTargets(targetSet);
+    }
+
+    bool NavigateToTarget(Transform targetTransform)
+    {
+        Vector3 targetDirection = (targetTransform.position - transform.position);
+        Vector2 stragithToTarget = Vector2.zero;
+        if (targetDirection.magnitude < .5f || isColliding)
         {
-            Vector3 targetDirection = (target.transform.position - transform.position);
-            if (targetDirection.magnitude < 2.5f)
-            {
-                target = null;
-                MoveAround(0f, 0f);
-                return;
-            }   
-
-            angleBetweenVectors = Vector3.SignedAngle(transform.forward,targetDirection,Vector3.up);
-
-            Vector2 stragithToTarget = new Vector2(angleBetweenVectors / Mathf.Abs(angleBetweenVectors),0.5f);
-
+            target = null;
             MoveAround(stragithToTarget.x, stragithToTarget.y);
-
-            
+            return true;
         }
+        else
+        {
+            angleBetweenVectors = Vector3.SignedAngle(transform.forward, targetDirection, Vector3.up);
+            stragithToTarget = new Vector2(angleBetweenVectors / Mathf.Abs(angleBetweenVectors), speedFactor);
+        }
+        MoveAround(stragithToTarget.x, stragithToTarget.y);
+        return false;
+    }
+
+    void NavigateThroughTargets(Transform[] targets)
+    {
+        if (targets.Length < 1) return;
+
+        if (targets.Length == 1) NavigateToTarget(targets[0]);
+        else if(currentTarget < targets.Length)
+            if (NavigateToTarget(targets[currentTarget])) currentTarget++;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isColliding = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        isColliding = false;
     }
 }
